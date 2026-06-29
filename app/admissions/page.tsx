bcd2303192b603ed5216762af55f78937f9c92ca
Subhrajeet Sahu <subhrajeetsau46@gmail.com>
2026-06-29 13:21:45 +0000

    feat: sync local work with repo

---
+++ app/admissions/page.tsx
@@ -0,0 +1,10 @@
+export default function AdmissionsPage() {
+  return (
+    <main className="min-h-screen pt-32 pb-24 px-6 max-w-[1280px] mx-auto">
+      <h1 className="font-display text-h2 text-maroon mb-8">Admissions 2025-26</h1>
+      <p className="font-body text-body-lg text-charcoal">
+        Information about our admission process will be updated here shortly.
+      </p>
+    </main>
+  )
+}
